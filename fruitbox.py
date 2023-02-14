b3516cb6cd1541bc8e8cbd34d96af630493f1a45
Ethan Kessler <7417274+ethanjkessler@users.noreply.github.com>
2023-02-13 20:29:15 -0700

    Break input automation out into its own file

--- fruitbox.py
+++ fruitbox.py
@@ -1,34 +1,9 @@
 
+import AutoInput
 import CSolver
 import OCR
-import pyautogui
 import PySolver
 import time
-
-def apply_matches(matches):
-    for match in matches:
-        start_point = match[0]
-        end_point = match[1]
-        start_x = OCR.BOARD_LEFT + (start_point.x * OCR.APPLE_DIMS[0])
-        start_y = OCR.BOARD_TOP + (start_point.y * OCR.APPLE_DIMS[1])
-        end_x = OCR.BOARD_LEFT + ((end_point.x + 1) * OCR.APPLE_DIMS[0])
-        end_y = OCR.BOARD_TOP + ((end_point.y + 1) * OCR.APPLE_DIMS[1])
-        pyautogui.moveTo(start_x, start_y)
-        pyautogui.mouseDown()
-        pyautogui.moveTo(end_x, end_y)
-        pyautogui.mouseUp()
-
-def reset():
-    reset_button = (40, 374)
-    play_button = (170, 217)
-    reset_x = OCR.BOARD_LEFT + reset_button[0]
-    reset_y = OCR.BOARD_TOP + reset_button[1]
-    play_x = OCR.BOARD_LEFT + play_button[0]
-    play_y = OCR.BOARD_TOP + play_button[1]
-    pyautogui.moveTo(reset_x, reset_y)
-    pyautogui.click()
-    pyautogui.moveTo(play_x, play_y)
-    pyautogui.click()
 
 def main():
     start_time = time.time()
@@ -47,7 +22,7 @@ def main():
     print(f'\nC Score: {c_score}')
     print(f'Py Score: {py_score}\n')
     apply_start = time.time()
-    apply_matches(c_matches)
+    AutoInput.apply_matches(c_matches)
     print(f'\ntime to apply: {time.time() - apply_start}')
     print(f'\nrun time: {time.time() - start_time}')
 
